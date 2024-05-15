f12c1d226bc17801d5c6c1987eb5dddf1094a315
Abhishek kumar <aasalex21c@gmail.com>
2024-05-15 17:13:18 +0530

    working on it

--- index.mjs
+++ index.mjs
@@ -1,1 +1,38 @@
-console.log('hello World!');
+import express from "express";
+// Creating a server
+  let server = express();
+  let port = 4000;
+
+// Sample products
+  let products = {
+    'Gold': "MMTC PAP 999.9 fine Gold (10gm)",
+    'Silver': "MMTC PAP 999.9 fine Gold (250gm)",
+    "TermInsurance": "LIC Term Insurance",
+    "EmergencyFund": "₹50 Lakhs"
+  }
+
+// i need to develop a middleware that will capture  essential information such as the
+// +  request method,
+// +  URL,
+// +  IP address, and
+// +  timestamps
+
+// using morgan
+function requestLogger(req, res, next){
+  console.log('middleware is called!');
+
+  // returning control to next middleware
+    next();
+}
+
+server.get('/products', (req, res)=>{
+
+  res.json(products);
+})
+
+server.use(requestLogger);
+
+// Listening 
+  server.listen(port, (req, res)=>{
+    console.log(`Server is running at port ${port}`);
+  });
